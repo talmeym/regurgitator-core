6ebbaefce66ce320790998b8a94f0a8230dd9a30
talmeym <mtalmey@gmail.com>
2015-06-30 11:22:14 +0100

    simplified caching

--- src/main/java/com/emarte/regurgitator/core/Caching.java
+++ src/main/java/com/emarte/regurgitator/core/Caching.java
@@ -7,37 +7,10 @@ public class Caching {
 	private static Map<Object, Object> DEFAULT_CACHE = new HashMap<Object, Object>();
 	private static Cache cacheToUse;
 
-	public static Cache getCache(final Class clazz) {
+	public static Cache getCache(Class clazz) {
 		if(cacheToUse == null) {
 			log.debug("Using default cache");
-
-			cacheToUse = new Cache() {
-				@Override
-				public boolean hasValue(Object key) {
-					return DEFAULT_CACHE.containsKey(trueKey(key));
-				}
-
-				@Override
-				public Object getValue(Object key) {
-					String trueKey = trueKey(key);
-					log.debug("Retrieving using true key of '" + trueKey + "'");
-					return DEFAULT_CACHE.get(trueKey);
-				}
-
-				@Override
-				public Object setValue(Object key, Object value) {
-					String trueKey = trueKey(key);
-					log.debug("Setting with true key of '" + trueKey + "'");
-					Object currentValue = DEFAULT_CACHE.get(trueKey);
-					DEFAULT_CACHE.put(trueKey, value);
-					return currentValue;
-				}
-
-				private String trueKey(Object key) {
-					String trueKey = clazz.getName() + ":" + key.toString();
-					return trueKey;
-				}
-			};
+			cacheToUse = buildDefaultCache(clazz);
 		}
 
 		return cacheToUse;
@@ -45,6 +18,36 @@ public class Caching {
 
 	public static void setCache(Cache cache) {
 		cacheToUse = cache;
+	}
+
+	private static Cache buildDefaultCache(final Class clazz) {
+		return new Cache() {
+			@Override
+			public boolean hasValue(Object key) {
+				return DEFAULT_CACHE.containsKey(trueKey(key));
+			}
+
+			@Override
+			public Object getValue(Object key) {
+				String trueKey = trueKey(key);
+				log.debug("Retrieving using true key of '" + trueKey + "'");
+				return DEFAULT_CACHE.get(trueKey);
+			}
+
+			@Override
+			public Object setValue(Object key, Object value) {
+				String trueKey = trueKey(key);
+				log.debug("Setting with true key of '" + trueKey + "'");
+				Object currentValue = DEFAULT_CACHE.get(trueKey);
+				DEFAULT_CACHE.put(trueKey, value);
+				return currentValue;
+			}
+
+			private String trueKey(Object key) {
+				String trueKey = clazz.getName() + ":" + key.toString();
+				return trueKey;
+			}
+		};
 	}
 
 	public static interface Cache {
