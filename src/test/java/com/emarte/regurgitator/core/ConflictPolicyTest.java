fd1c05f5791a3bb1c6b67ce434bea36f73e029e5
talmeym <mtalmey@gmail.com>
2015-09-18 09:14:02 +0100

    added extra test

--- src/test/java/com/emarte/regurgitator/core/ConflictPolicyTest.java
+++ src/test/java/com/emarte/regurgitator/core/ConflictPolicyTest.java
@@ -2,8 +2,12 @@ package com.emarte.regurgitator.core;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static com.emarte.regurgitator.core.ConflictPolicy.*;
+import static com.emarte.regurgitator.core.CoreTypes.LIST_OF_STRING;
 import static com.emarte.regurgitator.core.CoreTypes.STRING;
+import static java.util.Arrays.asList;
 
 public class ConflictPolicyTest {
 
@@ -13,5 +17,7 @@ public class ConflictPolicyTest {
 		REPLACE.resolveConflict("existing", "new", STRING).equals("new");
 		CONCAT.resolveConflict("existing", "new", STRING).equals("existingnew");
 		REMOVE.resolveConflict("existing", "ist", STRING).equals("exing");
+
+		REMOVE.resolveConflict(asList("1", "2", "3", "3"), asList("1", "2", "3"), LIST_OF_STRING	).equals(asList("3"));
 	}
 }
