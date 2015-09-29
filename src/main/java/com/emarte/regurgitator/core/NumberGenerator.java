a2b77600e50e09648522921c189ace04a3ee8114
talmeym <mtalmey@gmail.com>
2015-09-29 10:01:26 +0100

    added a configurable part to number generator, removed convenience interfaces from xml loading

--- src/main/java/com/emarte/regurgitator/core/NumberGenerator.java
+++ src/main/java/com/emarte/regurgitator/core/NumberGenerator.java
@@ -5,8 +5,18 @@ import java.util.Random;
 public class NumberGenerator implements ValueGenerator {
 	private static Random RANDOM = new Random();
 
+	private final Integer max;
+
+	public NumberGenerator(int max) {
+		this.max = max;
+	}
+
+	public NumberGenerator() {
+		max = null;
+	}
+
 	@Override
 	public Object generate() throws RegurgitatorException {
-		return RANDOM.nextLong();
+		return max != null ? (long) RANDOM.nextInt(max) : RANDOM.nextLong();
 	}
 }
