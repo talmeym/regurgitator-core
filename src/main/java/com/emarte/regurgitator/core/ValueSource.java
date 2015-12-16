94cf101f234f10b8d449115b0823d28c362c398e
talmeym <mtalmey@gmail.com>
2015-12-16 16:53:10 +0000

    added ValueSource to handle param sources with static defaults

--- src/main/java/com/emarte/regurgitator/core/ValueSource.java
+++ src/main/java/com/emarte/regurgitator/core/ValueSource.java
@@ -3,11 +3,11 @@ package com.emarte.regurgitator.core;
 public class ValueSource {
 
 	private final ContextLocation source;
-	private final String staticValue;
+	private final String value;
 
-	public ValueSource(ContextLocation source, String staticValue) {
+	public ValueSource(ContextLocation source, String value) {
 		this.source = source;
-		this.staticValue = staticValue;
+		this.value = value;
 	}
 
 	public Object getValue(Message message, Log log) throws RegurgitatorException {
@@ -17,15 +17,15 @@ public class ValueSource {
 			if (parameter != null) {
 				log.debug("Retrieved value from context location '" + source + "'");
 				return parameter.getValue();
-			} else if (staticValue != null) {
-				log.debug("defaulting to static value '" + staticValue + "'");
-				return staticValue;
+			} else if (value != null) {
+				log.debug("defaulting to static value '" + value + "'");
+				return value;
 			}
 
 			throw new RegurgitatorException("No value found at context location '" + source + "'");
 		}
 
-		log.debug("Using static value '" + staticValue + "'");
-		return staticValue;
+		log.debug("Using static value '" + value + "'");
+		return value;
 	}
 }
