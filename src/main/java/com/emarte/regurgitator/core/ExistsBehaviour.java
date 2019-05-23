34cdc29b559e43c964c181badb41114f2957206f
talmeym <mtalmey@gmail.com>
2019-05-23 09:23:13 +0100

    tidy up

--- src/main/java/com/emarte/regurgitator/core/ExistsBehaviour.java
+++ src/main/java/com/emarte/regurgitator/core/ExistsBehaviour.java
@@ -5,13 +5,14 @@
 package com.emarte.regurgitator.core;
 
 import static com.emarte.regurgitator.core.Log.getLog;
+import static java.lang.Boolean.parseBoolean;
 
 public final class ExistsBehaviour implements ConditionBehaviour {
     private static final Log log = getLog(ExistsBehaviour.class);
 
     @Override
     public boolean evaluate(Parameter parameter, Message message, String conditionValue, boolean notUsed) {
-        Boolean expectation = Boolean.valueOf(conditionValue);
+        boolean expectation = parseBoolean(conditionValue);
         boolean exists = parameter != null;
         log.debug("Parameter " + (exists ? "exists" : "does not exist"));
         return exists == expectation;
