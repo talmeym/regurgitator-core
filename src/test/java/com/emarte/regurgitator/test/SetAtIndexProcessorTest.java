8385ba6941bdbfc1b3d88bda7d1421301d5b9499
talmeym <mtalmey@gmail.com>
2019-06-05 14:24:15 +0100

    added set at index processor

---
+++ src/test/java/com/emarte/regurgitator/test/SetAtIndexProcessorTest.java
@@ -0,0 +1,7 @@
+package com.emarte.regurgitator.test;
+
+import com.emarte.regurgitator.core.SetAtIndexProcessor;
+
+public class SetAtIndexProcessorTest {
+    private SetAtIndexProcessor processor = new SetAtIndexProcessor(null, null);
+}
